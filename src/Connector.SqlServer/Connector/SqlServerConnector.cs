9cff93f4e11d93672db64408dbe3e62a1368185e
Ben Fidge <ben.fidge@btopenworld.com>
2020-10-16 11:15:58 +0700

    Get Data Types implemented

--- src/Connector.SqlServer/Connector/SqlServerConnector.cs
+++ src/Connector.SqlServer/Connector/SqlServerConnector.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using CluedIn.Core;
 using CluedIn.Core.Connectors;
+using CluedIn.Core.Data.Vocabularies;
 using CluedIn.Core.DataStore;
 using Microsoft.Data.SqlClient;
+using RabbitMQ.Client.Apigen.Attributes;
 
 namespace CluedIn.Connector.SqlServer.Connector
 {
@@ -36,7 +38,7 @@ namespace CluedIn.Connector.SqlServer.Connector
             foreach (System.Data.DataRow row in tables.Rows)
             {
                 var tableName = row["TABLE_NAME"] as string;
-                result.Add(new SqlServerConnectorContainer {Id = tableName, Name = tableName});
+                result.Add(new SqlServerConnectorContainer { Id = tableName, Name = tableName });
             }
 
             return result;
@@ -47,7 +49,7 @@ namespace CluedIn.Connector.SqlServer.Connector
             var cnxString = new SqlConnectionStringBuilder
             {
                 Authentication = SqlAuthenticationMethod.SqlPassword,
-                Password = (string) config.Authentication[SqlServerConstants.KeyName.Password],
+                Password = (string)config.Authentication[SqlServerConstants.KeyName.Password],
                 UserID = (string)config.Authentication[SqlServerConstants.KeyName.Username],
                 DataSource = (string)config.Authentication[SqlServerConstants.KeyName.Host],
                 InitialCatalog = (string)config.Authentication[SqlServerConstants.KeyName.DatabaseName],
@@ -59,9 +61,75 @@ namespace CluedIn.Connector.SqlServer.Connector
             return result;
         }
 
-        public override Task<IEnumerable<IConnectionDataType>> GetDataTypes(ExecutionContext executionContext, Guid providerDefinitionId, string containerId)
+        public override async Task<IEnumerable<IConnectionDataType>> GetDataTypes(ExecutionContext executionContext, Guid providerDefinitionId, string containerId)
         {
-            throw new NotImplementedException();
+            var config = await base.GetAuthenticationDetails(executionContext, providerDefinitionId);
+            var connection = await GetConnection(config);
+
+            var restrictions = new string[4];
+            restrictions[2] = containerId;
+            var tables = connection.GetSchema("Columns", restrictions);
+
+            var result = new List<SqlServerConnectorDataType>();
+            foreach (System.Data.DataRow row in tables.Rows)
+            {
+                var name = row["COLUMN_NAME"] as string;
+                var rawType = row["DATA_TYPE"] as string;
+                var type = GetVocabType(rawType);
+                result.Add(new SqlServerConnectorDataType { Name = name, RawDataType = rawType, Type = type });
+            }
+
+            return result;
+        }
+
+        private VocabularyKeyDataType GetVocabType(string rawType)
+        {
+            switch (rawType.ToLower())
+            {
+                case "bigint":
+                case "int":
+                case "smallint":
+                case "tinyint":
+                    return VocabularyKeyDataType.Integer;
+                case "bit":
+                    return VocabularyKeyDataType.Boolean;
+                case "decimal":
+                case "numeric":
+                case "float":
+                case "real":
+                    return VocabularyKeyDataType.Number;
+                case "money":
+                case "smallmoney":
+                    return VocabularyKeyDataType.Money;
+                case "datetime":
+                case "smalldatetime":
+                case "date":
+                case "datetimeoffset":
+                case "datetime2":
+                    return VocabularyKeyDataType.DateTime;
+                case "time":
+                    return VocabularyKeyDataType.Time;
+                case "char":
+                case "varchar":
+                case "text":
+                case "nchar":
+                case "nvarchar":
+                case "ntext":
+                    return VocabularyKeyDataType.Text;
+                case "XML":
+                    return VocabularyKeyDataType.Xml;
+                case "binary":
+                case "varbinary":
+                case "image":
+                case "timestamp":
+                case "uniqueidentifier":
+                    return VocabularyKeyDataType.Guid;
+                case "geometry":
+                case "geography":
+                    return VocabularyKeyDataType.GeographyLocation;
+                default:
+                    return VocabularyKeyDataType.Text;
+            }
         }
 
         public override Task<bool> VerifyConnection(ExecutionContext executionContext, Guid providerDefinitionId)
---
+++ src/Connector.SqlServer/Connector/SqlServerConnectorDataType.cs
@@ -0,0 +1,11 @@
+﻿using CluedIn.Core.Connectors;
+using CluedIn.Core.Data.Vocabularies;
+
+namespace CluedIn.Connector.SqlServer.Connector
+{
+    public class SqlServerConnectorDataType : IConnectionDataType {
+        public string Name { get; set; }
+        public VocabularyKeyDataType Type { get; set; }
+        public string RawDataType { get; set; }
+    }
+}
