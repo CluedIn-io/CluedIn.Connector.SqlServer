6a99cc6722f50bc44a472b9f485daf7c6056029e
Ben Fidge <ben.fidge@btopenworld.com>
2021-04-30 08:14:15 +0700

    Added feature for Create Container, and ability to specify Key Fields used when merging in StoreData

---
+++ src/Connector.SqlServer/Features/DefaultBuildCreateContainerFeature.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CluedIn.Connector.SqlServer.Connector;
+using CluedIn.Core.Connectors;
+using CluedIn.Core.Data.Vocabularies;
+
+namespace CluedIn.Connector.SqlServer.Features
+{
+    public class DefaultBuildCreateContainerFeature : IBuildCreateContainerFeature
+    {
+        public virtual string BuildCreateContainerSql(string name, IEnumerable<ConnectionDataType> columns)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine($"CREATE TABLE [{SqlSanitizer.Sanitize(name)}](");
+            builder.AppendJoin(", ", columns.Select(c => $"[{SqlSanitizer.Sanitize(c.Name)}] {GetDbType(c.Type)} NULL"));
+            builder.AppendLine(") ON[PRIMARY]");
+
+            var sql = builder.ToString();
+            return sql;
+        }
+
+        protected virtual string GetDbType(VocabularyKeyDataType type)
+        {
+            // return type switch //TODO: @LJU: Disabled as it needs reviewing; Breaks streams;
+            // {
+            //     VocabularyKeyDataType.Integer => "bigint",
+            //     VocabularyKeyDataType.Number => "decimal(18,4)",
+            //     VocabularyKeyDataType.Money => "money",
+            //     VocabularyKeyDataType.DateTime => "datetime2",
+            //     VocabularyKeyDataType.Time => "time",
+            //     VocabularyKeyDataType.Xml => "XML",
+            //     VocabularyKeyDataType.Guid => "uniqueidentifier",
+            //     VocabularyKeyDataType.GeographyLocation => "geography",
+            //     _ => "nvarchar(max)"
+            // };
+
+            return "nvarchar(max)";
+        }
+    }
+}
---
+++ src/Connector.SqlServer/Features/IBuildCreateContainerFeature.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CluedIn.Core.Connectors;
+
+namespace CluedIn.Connector.SqlServer.Features
+{
+    public interface IBuildCreateContainerFeature
+    {
+        string BuildCreateContainerSql(string containerName, IEnumerable<ConnectionDataType> columns);
+    }
+}
