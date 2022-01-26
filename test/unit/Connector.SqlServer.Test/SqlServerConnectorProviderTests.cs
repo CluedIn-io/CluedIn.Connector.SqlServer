9ae0f0851efaac54794cc31e8d4a9f0f77130679
Kieranties <Kieran@Kieranties.com>
2022-01-26 14:49:55 +0000

    fix: Correct tests based on new common connector implementation

--- test/unit/Connector.SqlServer.Test/SqlServerConnectorProviderTests.cs
+++ test/unit/Connector.SqlServer.Test/SqlServerConnectorProviderTests.cs
@@ -37,34 +37,36 @@ namespace CluedIn.Connector.SqlServer.Unit.Tests
         }
 
         [Theory, AutoData]
-        public async void GetCrawlJobData_NullContext_ReturnsDefaults(Guid orgId, Guid userId, Guid providerDefId)
+        public async void GetCrawlJobData_NullContext_ReturnsGiven(Guid orgId, Guid userId, Guid providerDefId)
         {
             var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
 
-            var result = await sut.GetCrawlJobData(null, new Dictionary<string, object>(), orgId, userId, providerDefId);
+            var result = await sut.GetCrawlJobData(null, new Dictionary<string, object>()
+            {
+                { "alpha", "one" },
+                { "beta", 2 }
+            }, orgId, userId, providerDefId);
 
             result.Should().BeOfType<CrawlJobDataWrapper>();
             var typedResult = result as CrawlJobDataWrapper;
             typedResult.Configurations.Should().Equal(
                 new Dictionary<string, object>
                 {
-                    { SqlServerConstants.KeyName.Username, null },
-                    { SqlServerConstants.KeyName.DatabaseName, null },
-                    { SqlServerConstants.KeyName.Host, null },
-                    { SqlServerConstants.KeyName.Password, null },
-                    { SqlServerConstants.KeyName.PortNumber, 1433 }
+                    { "alpha", "one" },
+                    { "beta", 2 }
                 });
         }
 
         [Theory, AutoData]
-        public void GetCrawlJobData_NullConfiguration_Throws(Guid orgId, Guid userId, Guid providerDefId)
+        public async void GetCrawlJobData_NullConfiguration_SetsNull(Guid orgId, Guid userId, Guid providerDefId)
         {
             var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
 
-            Func<Task> action = () => sut.GetCrawlJobData(_testContext.ProviderUpdateContext, null, orgId, userId, providerDefId);
+            var result = await sut.GetCrawlJobData(_testContext.ProviderUpdateContext, null, orgId, userId, providerDefId);
 
-            action.Should().Throw<ArgumentNullException>()
-                .And.ParamName.Should().Be("configuration");
+            result.Should().BeOfType<CrawlJobDataWrapper>();
+            var typedResult = result as CrawlJobDataWrapper;
+            typedResult.Configurations.Should().BeNull();
         }
 
         [Theory, AutoData]
@@ -118,7 +120,7 @@ namespace CluedIn.Connector.SqlServer.Unit.Tests
         }
 
         [Theory, AutoData]
-        public async void GetHelperConfiguration_NullContext_ReturnsDefaults(Guid orgId, Guid userId, Guid providerDefId)
+        public async void GetHelperConfiguration_NullContext_ReturnsEmpty(Guid orgId, Guid userId, Guid providerDefId)
         {
             var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
             var data = new CrawlJobDataWrapper(new Dictionary<string, object>());
@@ -126,26 +128,17 @@ namespace CluedIn.Connector.SqlServer.Unit.Tests
 
             var result = await sut.GetHelperConfiguration(null, data, orgId, userId, providerDefId);
 
-            result.Should().Equal(
-                new Dictionary<string, object>
-                {
-                    { SqlServerConstants.KeyName.Username, null },
-                    { SqlServerConstants.KeyName.DatabaseName, null },
-                    { SqlServerConstants.KeyName.Host, null },
-                    { SqlServerConstants.KeyName.Password, null },
-                    { SqlServerConstants.KeyName.PortNumber, 1433 }
-                });
+            result.Should().BeEmpty();
         }
 
         [Theory, AutoData]
-        public void GetHelperConfiguration_NullData_Throws(Guid orgId, Guid userId, Guid providerDefId)
+        public async void GetHelperConfiguration_NullData_Throws(Guid orgId, Guid userId, Guid providerDefId)
         {
             var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
 
-            Func<Task> action = () => sut.GetHelperConfiguration(_testContext.ProviderUpdateContext, null, orgId, userId, providerDefId);
+            var result = await sut.GetHelperConfiguration(_testContext.ProviderUpdateContext, null, orgId, userId, providerDefId);            
 
-            action.Should().Throw<ArgumentNullException>()
-                .And.ParamName.Should().Be("jobData");
+            result.Should().BeEmpty();
         }
 
         [Theory, AutoData]
@@ -176,85 +169,41 @@ namespace CluedIn.Connector.SqlServer.Unit.Tests
         }
 
         [Theory, AutoData]
-        public async void GetHelperConfigurationWithFolder_NullContext_ReturnsDefaults(Guid orgId, Guid userId, Guid providerDefId, string folderId)
+        public void GetHelperConfigurationWithFolder_Throws_NotImplemented(Guid orgId, Guid userId, Guid providerDefId, string folderId)
         {
             var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
-            var data = new CrawlJobDataWrapper(new Dictionary<string, object>());
+
+            Func<Task> action = () => sut.GetHelperConfiguration(null, null, orgId, userId, providerDefId, folderId);
+
+            action.Should().Throw<NotImplementedException>();
+        }
 
 
-            var result = await sut.GetHelperConfiguration(null, data, orgId, userId, providerDefId, folderId);
+        [Theory, AutoData]
+        public async void GetAccountInformation_NullData_ReturnsEmpty(Guid orgId, Guid userId, Guid providerDefId)
+        {
+            var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
 
-            result.Should().Equal(
-                new Dictionary<string, object>
-                {
-                    { SqlServerConstants.KeyName.Username, null },
-                    { SqlServerConstants.KeyName.DatabaseName, null },
-                    { SqlServerConstants.KeyName.Host, null },
-                    { SqlServerConstants.KeyName.Password, null },
-                    { SqlServerConstants.KeyName.PortNumber, 1433 }
-                });
+            var result = await sut.GetAccountInformation(
+                _testContext.ProviderUpdateContext, null, orgId, userId, providerDefId);
+
+            result.AccountId.Should().Be(string.Empty);
+            result.AccountId.Should().Be(string.Empty);
         }
 
         [Theory, AutoData]
-        public void GetHelperConfigurationWithFolder_NullData_Throws(Guid orgId, Guid userId, Guid providerDefId, string folderId)
+        public async void GetAccountInformation_InvalidJobDataType_ReturnsEmpty(Guid orgId, Guid userId, Guid providerDefId)
         {
             var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
 
-            Func<Task> action = () => sut.GetHelperConfiguration(_testContext.ProviderUpdateContext, null, orgId, userId, providerDefId, folderId);
+            var data = new CrawlJobData();
 
-            action.Should().Throw<ArgumentNullException>()
-                .And.ParamName.Should().Be("jobData");
-        }
+            var result = await sut.GetAccountInformation(
+                _testContext.ProviderUpdateContext, data, orgId, userId, providerDefId);
 
-        [Theory, AutoData]
-        public async void GetHelperConfigurationWithFolder_CamelCaseKeys_MatchesConstantsAndReturnsValues(Guid orgId, Guid userId, Guid providerDefId, string folderId)
-        {
-            var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
-            var data = new CrawlJobDataWrapper(new Dictionary<string, object> {
-                { "username", "user" },
-                { "databaseName", "database" },
-                { "host", "host" },
-                { "password", "password" },
-                { "portNumber", 1234 }
-            });
+            result.AccountId.Should().Be(string.Empty);
+            result.AccountId.Should().Be(string.Empty);
 
-
-
-            var result = await sut.GetHelperConfiguration(_testContext.ProviderUpdateContext, data, orgId, userId, providerDefId, folderId);
-
-            result.Should().Equal(
-                new Dictionary<string, object>
-                {
-                    { SqlServerConstants.KeyName.Username, "user" },
-                    { SqlServerConstants.KeyName.DatabaseName, "database" },
-                    { SqlServerConstants.KeyName.Host, "host" },
-                    { SqlServerConstants.KeyName.Password, "password" },
-                    { SqlServerConstants.KeyName.PortNumber, 1234 }
-                });
-        }
-
-        [Theory, AutoData]
-        public void GetAccountInformation_NullData_Throws(Guid orgId, Guid userId, Guid providerDefId)
-        {
-            var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
-
-            Func<Task> action = () => sut.GetAccountInformation(_testContext.ProviderUpdateContext, null, orgId, userId, providerDefId);
-
-            action.Should().Throw<ArgumentNullException>()
-                .And.ParamName.Should().Be("jobData");
-        }
-
-        [Theory, AutoData]
-        public void GetAccountInformation_InvalidJobDataType_Throws(Guid orgId, Guid userId, Guid providerDefId)
-        {
-            var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
-
-            Func<Task> action = () => sut.GetAccountInformation(
-                _testContext.ProviderUpdateContext, new CrawlJobData(), orgId, userId, providerDefId);
-
-            action.Should().Throw<ArgumentException>()
-                .And.ParamName.Should().Be("jobData");
-                
         }
 
         [Theory, AutoData]
@@ -266,8 +215,8 @@ namespace CluedIn.Connector.SqlServer.Unit.Tests
             var result = await sut.GetAccountInformation(
                 _testContext.ProviderUpdateContext, data, orgId, userId, providerDefId);
 
-            result.AccountId.Should().Be(".");
-            result.AccountId.Should().Be(".");
+            result.AccountId.Should().BeEmpty();
+            result.AccountId.Should().BeEmpty();
         }
 
         [Theory, AutoData]
@@ -285,8 +234,8 @@ namespace CluedIn.Connector.SqlServer.Unit.Tests
             var result = await sut.GetAccountInformation(
                 _testContext.ProviderUpdateContext, data, orgId, userId, providerDefId);
 
-            result.AccountId.Should().Be("host.database");
-            result.AccountId.Should().Be("host.database");
+            result.AccountId.Should().BeEmpty();
+            result.AccountDisplay.Should().Be("host database ");
         }
 
         [Theory]
@@ -299,39 +248,6 @@ namespace CluedIn.Connector.SqlServer.Unit.Tests
             var result = sut.Schedule(dateTime, webhooksEnabled);
 
             result.Should().Be($"{dateTime.Minute} 0/23 * * *");
-        }
-
-        [Fact]
-        public void CreateWebHook_NullJobData_Throws()
-        {
-            var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
-
-            Func<Task> action = () => sut.CreateWebHook(null, null, Mock.Of<IWebhookDefinition>(), new Dictionary<string, object>());
-
-            action.Should().Throw<ArgumentNullException>()
-                .And.ParamName.Should().Be("jobData");
-        }
-
-        [Fact]
-        public void CreateWebHook_NullWebHookDefinition_Throws()
-        {
-            var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
-
-            Func<Task> action = () => sut.CreateWebHook(null, new CrawlJobData(), null, new Dictionary<string, object>());
-
-            action.Should().Throw<ArgumentNullException>()
-                .And.ParamName.Should().Be("webhookDefinition");
-        }
-
-        [Fact]
-        public void CreateWebHook_NullConfig_Throws()
-        {
-            var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
-
-            Func<Task> action = () => sut.CreateWebHook(null, new CrawlJobData(), Mock.Of<IWebhookDefinition>(), null);
-
-            action.Should().Throw<ArgumentNullException>()
-                .And.ParamName.Should().Be("config");
         }
 
         [Fact]
@@ -355,28 +271,6 @@ namespace CluedIn.Connector.SqlServer.Unit.Tests
         }
 
         [Fact]
-        public void DeleteWebHook_NullJobData_Throws()
-        {
-            var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
-
-            Func<Task> action = () => sut.DeleteWebHook(null, null, Mock.Of<IWebhookDefinition>());
-
-            action.Should().Throw<ArgumentNullException>()
-                .And.ParamName.Should().Be("jobData");
-        }
-
-        [Fact]
-        public void DeleteWebHook_NullWebHookDefinition_Throws()
-        {
-            var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
-
-            Func<Task> action = () => sut.DeleteWebHook(null, new CrawlJobData(), null);
-
-            action.Should().Throw<ArgumentNullException>()
-                .And.ParamName.Should().Be("webhookDefinition");
-        }
-
-        [Fact]
         public void DeleteWebHook_Throws_NotImplemented()
         {
             var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
@@ -387,17 +281,6 @@ namespace CluedIn.Connector.SqlServer.Unit.Tests
         }
 
         [Fact]
-        public void WebhookManagementEndpoints_NullIds_Throws()
-        {
-            var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
-
-            Action action = () => sut.WebhookManagementEndpoints(null);
-
-            action.Should().Throw<ArgumentNullException>()
-                .And.ParamName.Should().Be("ids");
-        }
-
-        [Fact]
         public void WebhookManagementEndpoints_Throws_NotImplemented()
         {
             var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
@@ -405,17 +288,6 @@ namespace CluedIn.Connector.SqlServer.Unit.Tests
             Action action = () => sut.WebhookManagementEndpoints(Array.Empty<string>());
 
             action.Should().Throw<NotImplementedException>();
-        }
-
-        [Theory, AutoData]
-        public void GetRemainingApiAllowance_NullJobData_Throws(Guid orgId, Guid userId, Guid providerDefId)
-        {
-            var sut = new SqlServerConnectorProvider(_testContext.AppContext.Object, _constants, _logger);
-
-            Action action = () => sut.GetRemainingApiAllowance(null, null, orgId, userId, providerDefId);
-
-            action.Should().Throw<ArgumentNullException>()
-                .And.ParamName.Should().Be("jobData");
         }
 
         [Theory, AutoData]
