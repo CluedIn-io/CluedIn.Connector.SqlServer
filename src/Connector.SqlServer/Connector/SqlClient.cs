0f02ca80aeed3f5e7d39b8e79c99bb84bd730def
Andrew Hardy <aha@cluedin.com>
2022-12-08 14:07:47 +1000

    feat: Retry if getting a connection from the pool fails due to a timeout

--- src/Connector.SqlServer/Connector/SqlClient.cs
+++ src/Connector.SqlServer/Connector/SqlClient.cs
@@ -2,12 +2,31 @@
 using CluedIn.Connector.Common.Configurations;
 using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CluedIn.Connector.SqlServer.Connector
 {
     public class SqlClient : ClientBase<SqlConnection, SqlParameter>, ISqlClient
     {
         private readonly int _defaultPort = 1433;
+
+        public override async Task<SqlConnection> GetConnection(IDictionary<string, object> config)
+        {
+            while (true)
+            {
+                try
+                {
+                    return await base.GetConnection(config);
+                }
+                catch (System.InvalidOperationException ex)
+                {
+                    if (ex.ToString().Contains("The timeout period elapsed prior to obtaining a connection from the pool"))
+                        await Task.Delay(1000);
+                    else
+                        throw;
+                }
+            }
+        }
 
         public override string BuildConnectionString(IDictionary<string, object> config)
         {
