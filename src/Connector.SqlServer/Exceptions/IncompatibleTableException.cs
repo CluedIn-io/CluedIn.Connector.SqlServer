019b0a585612f1961f609446afc9c8811d734e2b
Andreas Nicolaisen <39338793+AndreasNicolaisen@users.noreply.github.com>
2023-06-29 14:17:18 +0200

    feat: Throw exception if old table exists (#78)

---
+++ src/Connector.SqlServer/Exceptions/IncompatibleTableException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CluedIn.Connector.SqlServer.Exceptions
+{
+    internal class IncompatibleTableException : Exception
+    {
+        public IncompatibleTableException(string message)
+            : base($"Table is incompatible: {message}")
+        {
+
+        }
+
+        public static IncompatibleTableException OldTableVersionExists(Guid streamId, Guid connectorProviderDefinitionId)
+        {
+            var message = $"""
+
+                StreamId: {streamId}
+                ConnectorProviderDefinitionId: {connectorProviderDefinitionId}
+
+                It looks like the tables in the database is not compatible with this version of the connector.
+                This is most likely because the tables were created in an older version of the connector.
+                To remedy:
+                    - Disable all streams using this connector
+                    - Reprocess the streams
+                    - Reenable the streams
+                """;
+
+            return new IncompatibleTableException(message);
+        }
+    }
+}
