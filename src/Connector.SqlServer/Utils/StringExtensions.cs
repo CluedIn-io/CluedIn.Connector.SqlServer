ed7238f0927d29da74810f8378bab75f52180274
Andreas Nicolaisen <ann@cluedin.com>
2023-12-12 14:14:16 +0100

    fix: Prepend table names starting with digit

--- src/Connector.SqlServer/Utils/StringExtensions.cs
+++ src/Connector.SqlServer/Utils/StringExtensions.cs
@@ -9,7 +9,14 @@ namespace CluedIn.Connector.SqlServer.Utils
         /// </summary>
         public static string ToSanitizedSqlName(this string value)
         {
-            return Regex.Replace(value, @"[^_A-Za-z0-9]+", string.Empty);
+            var withoutSpecialCharacters = Regex.Replace(value, @"[^_A-Za-z0-9]+", string.Empty);
+
+            if (Regex.IsMatch(withoutSpecialCharacters, @"^\d.*"))
+            {
+                return $"Table{withoutSpecialCharacters}";
+            }
+
+            return withoutSpecialCharacters;
         }
     }
 }
---
+++ test/unit/Connector.SqlServer.Test/Utils/StringExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using AutoFixture.Xunit2;
+using CluedIn.Connector.SqlServer.Utils;
+using FluentAssertions;
+using Xunit;
+
+namespace CluedIn.Connector.SqlServer.Unit.Tests.Utils;
+
+public class StringExtensionsTests
+{
+    [Theory]
+    [InlineAutoData("TableName", "TableName")]
+    [InlineAutoData("TableName1", "TableName1")]
+    [InlineAutoData("TableName+", "TableName")]
+    [InlineAutoData("+TableName", "TableName")]
+    [InlineAutoData("1", "Table1")]
+    [InlineAutoData("123", "Table123")]
+    [InlineAutoData("1Table", "Table1Table")]
+    public void ToSanitizedTableName_ShouldYieldName(string input, string expectedOutput)
+    {
+        // arrange
+        // act
+        var result = input.ToSanitizedSqlName();
+
+        // assert
+        result.Should().Be(expectedOutput);
+    }
+}
