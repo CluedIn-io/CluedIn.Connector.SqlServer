d0dc362786618f2209b77c3c62be30b146036335
Andreas Nicolaisen <ann@cluedin.com>
2023-12-12 14:38:00 +0100

    chore: Use Char.IsDigit instead of regex

--- src/Connector.SqlServer/Utils/StringExtensions.cs
+++ src/Connector.SqlServer/Utils/StringExtensions.cs
@@ -11,7 +11,8 @@ namespace CluedIn.Connector.SqlServer.Utils
         {
             var withoutSpecialCharacters = Regex.Replace(value, @"[^_A-Za-z0-9]+", string.Empty);
 
-            if (Regex.IsMatch(withoutSpecialCharacters, @"^\d.*"))
+            if (withoutSpecialCharacters.Length > 0 &&
+                char.IsDigit(withoutSpecialCharacters[0]))
             {
                 return $"Table{withoutSpecialCharacters}";
             }
