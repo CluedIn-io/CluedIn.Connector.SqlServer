a1c6f32f907565c2d07edfbe43941c68ae0ddc98
Andreas Nicolaisen <ann@cluedin.com>
2023-12-14 14:47:41 +0100

    fix: Cap length of column name

--- src/Connector.SqlServer/Utils/StringExtensions.cs
+++ src/Connector.SqlServer/Utils/StringExtensions.cs
@@ -22,7 +22,35 @@ namespace CluedIn.Connector.SqlServer.Utils
                 sanitizedSqlName = $"Table{sanitizedSqlName}";
             }
 
+            if (sanitizedSqlName.Length > 127) // 127 instead of 128, since we need space for parameter declaration '@'
+            {
+                var hashValue = Math.Abs(GetStableHashCode(value)).ToString().PadLeft(10, '0');
+                var postFix = $"_{hashValue}";
+                sanitizedSqlName = $"{sanitizedSqlName.Remove(127 - postFix.Length)}{postFix}";
+            }
+
             return sanitizedSqlName;
+        }
+
+        // # .NET implementation of string hash code copied from here:
+        // https://stackoverflow.com/a/36845864/2009373
+        private static int GetStableHashCode(string str)
+        {
+            unchecked
+            {
+                int hash1 = 5381;
+                int hash2 = hash1;
+
+                for (int i = 0; i < str.Length && str[i] != '\0'; i += 2)
+                {
+                    hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                    if (i == str.Length - 1 || str[i + 1] == '\0')
+                        break;
+                    hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+                }
+
+                return hash1 + (hash2 * 1566083941);
+            }
         }
     }
 }
--- test/unit/Connector.SqlServer.Test/Utils/StringExtensionsTests.cs
+++ test/unit/Connector.SqlServer.Test/Utils/StringExtensionsTests.cs
@@ -16,6 +16,7 @@ public class StringExtensionsTests
     [InlineAutoData("1", "Table1")]
     [InlineAutoData("123", "Table123")]
     [InlineAutoData("1Table", "Table1Table")]
+    [InlineAutoData("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa_0392220648")] // Expected result should not be changed, since we need to ensure that hash is stable
     public void ToSanitizedTableName_ShouldYieldName(string input, string expectedOutput)
     {
         // arrange
@@ -37,5 +38,19 @@ public class StringExtensionsTests
 
         // Assert
         action.Should().Throw<ArgumentException>();
+    }
+
+    [Theory]
+    [InlineAutoData("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")]
+    [InlineAutoData("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")]
+    [InlineAutoData("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc")]
+    public void ToSanitizedTableName_ShouldLimitSize(string input)
+    {
+        // arrange
+        // act
+        var result = input.ToSanitizedSqlName();
+
+        // assert
+        result.Length.Should().Be(127);
     }
 }
